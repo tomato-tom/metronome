c24011a42589e4f659f6be29e3787e30fb6706aa
tomato-tom <hajimeee49@gmail.com>
2026-02-07 00:48:20 +0900

    add pause function

--- metronome.sh
+++ metronome.sh
@@ -1,23 +1,55 @@
 #!/bin/bash
-# metronome.sh
 
 bpm=${1:-120}
-bars=${2:-4}
+beats=${2:-4}
+bars=${3:-99}
 interval=$(echo "scale=3; 60/$bpm" | bc)
 
 echo "BPM: $bpm (${interval}s per beat)"
-echo "Bars: $bars"
-echo "Press Ctrl+C to stop"
+echo "p to pause/resume, q to quit"
+echo
 
-trap 'echo -e "\nStopped"; exit' INT
+trap 'kill $play_pid 2>/dev/null; echo -e "\nStopped"; exit' INT
 
-current_bar=1
+play() {
+    local bar=1 beat=1
+    while [ $bar -le $bars ]; do
+        beat=1
+        while [ $beat -le $beats ]; do
+            # 一時停止ファイルをチェック
+            while [ -f /tmp/metronome_pause ]; do
+                sleep 0.1
+            done
+            
+            echo -en "== Bar: $bar Beat: $beat ==\r"
+            beep -l 60 &
+            sleep $interval
+            ((beat++))
+        done
+        ((bar++))
+    done
+}
 
-while [ $bars -gt 0 ]; do
-    echo -en "== Bar: $current_bar ==\r"
-    echo -ne '\a'
-    sleep $interval
+play &
+play_pid=$!
 
-    ((bars--))
-    ((current_bar++))
+paused=false
+while :; do
+    read -rsn1 key
+    if [ -z $key ]; then
+        if $paused; then
+            rm -f /tmp/metronome_pause
+            paused=false
+            echo -ne "\rResumed...              \r"
+        else
+            touch /tmp/metronome_pause
+            paused=true
+            echo -ne "\rPaused...               \r"
+        fi
+    elif [[ $key == 'q' ]]; then
+        kill $play_pid 2>/dev/null
+        rm -f /tmp/metronome_pause
+        echo -e "\nQuit"
+        exit
+    fi
 done
